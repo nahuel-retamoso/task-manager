ec9d12c72e5534dd9bbefaaa9145eb25d171bbf6
nahuel-retamoso <nahuel.retamoso@gmail.com>
2023-06-16 17:26:08 -0300

    navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Title</h1>
+            <ul>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>
+                <li>Item</li>           
+            </ul>    
+        </div>
+    )
+}
+
+export default Navbar;
