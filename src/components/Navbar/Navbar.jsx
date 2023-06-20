3b0f773255a6932e0815dd931c40dd3f4f36adbc
Valentin Virzi <v.virzi@hotmail.com>
2023-06-20 14:32:02 -0300

    navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,15 +2,37 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Title</h1>
-            <ul>
-                <li>Item</li>
-                <li>Item</li>
-                <li>Item</li>
-                <li>Item</li>           
-            </ul>    
-        </div>
+        <nav className='main-nav'>
+            <ul className='navbar'>
+                <li className='navbar__item'>
+                    <a className='navbar__item-a' href="/home.html">
+                        <img className='navbar__item-img' src="/assets/home.svg" alt="home icon" />
+                        <h3>Home</h3>
+                    </a>
+                </li>
+                <li className='navbar__item'>
+                    <a className='navbar__item-a' href="/agenda.html">
+                        <img className='navbar__item-img' src="/assets/note.svg" alt="agenda icon" />
+                        <h3>Agenda</h3>
+                    </a>
+                </li>
+                <button className='navbar__btn'>
+                    <img src="/assets/plus.svg" alt="add icon" />
+                </button>
+                <li className='navbar__item'>
+                    <a className='navbar__item-a' href="bienestar.html">
+                        <img className='navbar__item-img' src="/assets/circles.svg" alt="bienestar icon" />
+                        <h3>Bienestar</h3>
+                    </a>
+                </li>
+                <li className='navbar__item'>
+                    <a className='navbar__item-a' href="logros.html">
+                        <img className='navbar__item-img' src="/assets/crown.svg" alt="logros icon" />
+                        <h3>Logros</h3>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
